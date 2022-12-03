7a7c5b92cd96b9227c0c8d6d1fca73a42a40e5e5
OzimanD <89782267+OzimanD@users.noreply.github.com>
2022-12-03 18:02:30 +0200

    )))

---
+++ src/ClasessAndObjects/ex_001_constructors/declaration/Animal.java
@@ -0,0 +1,10 @@
+package ClasessAndObjects.ex_001_constructors.declaration;
+
+public class Animal {
+    private int age;
+
+    // оголошення конструктора
+    public Animal(int age) {
+        this.age = age;
+    }
+}
---
+++ src/ClasessAndObjects/ex_002_constructors/kw_this/Animal.java
@@ -0,0 +1,12 @@
+package ClasessAndObjects.ex_002_constructors.kw_this;
+
+public class Animal {
+    private int age;
+    private int height;
+
+    // робота ключового слова this
+    public Animal(int a, int h) {
+        age = a;
+        height = h;
+    }
+}
